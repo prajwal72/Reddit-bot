c4f9d4613e6d673a7f253b3cbc64be45974163b2
Prajwal <2017ugcs059@nitjsr.ac.in>
2019-03-24 00:57:58 +0530

    Only post with more than 1k upvotes are added

--- bot.py
+++ bot.py
@@ -14,16 +14,19 @@ for subreddit in subreddits:
         id = post.id
         if db.child("post_ids").child(id).get().val() == 1:
             continue
+        upvotes = post.ups
+        if upvotes < 1000:
+            continue
         title = post.title
         text = post.selftext
         url = post.url
         if "comments" in url:
             url = "null"
-        values = {
+        data = {
         "id" : id,
         "title" : title,
         "text" : text,
         "url" : url
         }
-        db.child("posts").child(subreddit).push(values)
+        db.child("posts").child(subreddit).push(data)
         db.child("post_ids").child(id).set(1)
