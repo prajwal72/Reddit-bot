1ec476e692fa95d40ff46f25fd5f1b453ab2aa30
Prajwal <2017ugcs059@nitjsr.ac.in>
2019-03-24 00:22:32 +0530

    Add bot.py

---
+++ bot.py
@@ -0,0 +1,29 @@
+import praw
+import pyrebase
+
+from config import config
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+reddit = praw.Reddit('bot1')
+subreddits = ['memes', 'jokes', 'funny']
+
+for subreddit in subreddits:
+    sub = reddit.subreddit(subreddit)
+    for post in sub.hot(limit = 100):
+        id = post.id
+        if db.child("post_ids").child(id).get().val() == 1:
+            continue
+        title = post.title
+        text = post.selftext
+        url = post.url
+        if "comments" in url:
+            url = "null"
+        values = {
+        "id" : id,
+        "title" : title,
+        "text" : text,
+        "url" : url
+        }
+        db.child("posts").child(subreddit).push(values)
+        db.child("post_ids").child(id).set(1)
